a396d1764156af254eb427b7701e3bd5b4064d18
Jared Forsyth <jared@jaredforsyth.com>
2020-02-18 07:35:59 -0700

    ready for some crdt?

--- packages/text-crdt/cli-editor.js
+++ packages/text-crdt/cli-editor.js
@@ -1,27 +1,58 @@
+// @flow
+require('@babel/register');
+const crdt = require('./tree');
+const chalk = require('chalk');
 var blessed = require('blessed'),
     program = blessed.program();
 
-const state = { text: '', mode: 'insert', pos: 0 };
+/*::
+type Format = {
+    bold?: boolean,
+    underline?: boolean,
+}
+type State = {
+    text: crdt.CRDT<Format>,
+    sel: {anchor: number, cursor: number},
+    mode: 'insert' | 'normal',
+}
+*/
 
-program.on('keypress', function(ch, evt) {
+const format = (text, format) => {
+    if (format.bold) {
+        text = chalk.bold(text);
+    }
+    if (format.underline) {
+        text = chalk.underline(text);
+    }
+    return text;
+};
+
+const draw = state => {
+    program.cursorShape(state.mode === 'insert' ? 'line' : 'block');
+    program.cursorColor('red');
+    program.move(0, 0);
+    program.eraseInLine(2);
+    // program.bg('red');
+    program.write(state.text ? chalk.bgRed(state.text) : '<type something>');
+    program.write(chalk.bold('blded'));
+    // program.bg('!red');
+    program.move(state.pos, 0);
+};
+
+const handleKeyPress = (state, ch, evt) => {
     if (evt.full === 'enter' || evt.full === 'return') {
         return;
     }
     if (state.mode === 'insert') {
         if (evt.full === 'escape') {
             state.mode = 'normal';
-            program.selData(2, 3);
-            debug(state);
             return;
         }
         if (evt.full === 'backspace') {
             state.pos -= 1;
-            program.move(state.pos, 0);
             state.text =
                 state.text.slice(0, state.pos) +
                 state.text.slice(state.pos + 1);
-            program.deleteChars(1);
-            debug(state);
             return;
         }
         if (!ch) {
@@ -29,10 +60,6 @@ program.on('keypress', function(ch, evt) {
         }
         state.text =
             state.text.slice(0, state.pos) + ch + state.text.slice(state.pos);
-        if (state.pos < state.text.length) {
-            program.insertChars(1);
-        }
-        program.write(ch);
         state.pos += 1;
     } else {
         if (ch === 'h' || evt.full === 'left') {
@@ -55,29 +82,31 @@ program.on('keypress', function(ch, evt) {
             state.mode = 'insert';
         }
         if (ch === 'x') {
-            program.deleteChars(1);
             state.text =
                 state.text.slice(0, state.pos) +
                 state.text.slice(state.pos + 1);
         }
     }
+};
+
+const state = { text: '', mode: 'insert', pos: 0 };
+
+program.on('keypress', function(ch, evt) {
+    handleKeyPress(state, ch, evt);
     debug(state);
-    program.move(state.pos, 0);
+    draw(state);
 });
 
 const debug = state => {
-    program.cursorShape(state.mode === 'insert' ? 'line' : 'block');
-    program.cursorColor(9);
     program.move(0, 5);
     program.eraseInLine(2);
     program.write(state.text);
-    program.move(state.pos, 0);
 };
 
 program.key('q', function(ch, key) {
     program.clear();
     program.disableMouse();
-    // program.showCursor();
+    program.cursorShape('block');
     program.normalBuffer();
     process.exit(0);
 });
@@ -91,17 +120,9 @@ program.on('mouse', function(data) {
     }
 });
 
-program.alternateBuffer();
-// program.enableMouse();
-// program.hideCursor();
-program.clear();
-
-program.move(0, 0);
-program.write('m');
-// program.bg('black');
-// program.write('Hello world', 'blue fg');
-// program.setx(((program.cols / 2) | 0) - 4);
-// program.down(5);
-// program.write('Hi again!');
-// program.bg('!black');
-program.feed();
+setTimeout(() => {
+    program.alternateBuffer();
+    program.clear();
+    draw(state);
+    program.feed();
+}, 5);
