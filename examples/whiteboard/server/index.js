17fc47276a59663f179bb648fab09688294dc18e
Jared Forsyth <jared@jaredforsyth.com>
2020-03-27 21:43:56 -0600

    ok I think the server is in decent shape

--- examples/whiteboard/server/index.js
+++ examples/whiteboard/server/index.js
@@ -8,9 +8,13 @@ import type {
 import make from '../../../packages/core/src/server';
 
 import setupPersistence from '../../../packages/server-bundle/sqlite-persistence';
-// import setupPersistence from '../../../packages/core/src/memory-persistence';
+import setupInMemoryPersistence from '../../../packages/core/src/memory-persistence';
 
-import { runServer } from '../../../packages/server-bundle';
+import {
+    runServer,
+    setupWebsocket,
+    setupPolling,
+} from '../../../packages/server-bundle';
 
 type Delta = NewDelta<any, null, any>;
 type Data = CRDT<any, null>;
@@ -25,5 +29,12 @@ const crdtImpl = {
 
 export const run = (dataPath: string, port: number = 9090) => {
     const server = make<Delta, Data>(crdtImpl, setupPersistence(dataPath));
-    return runServer(port, dataPath, server);
+    const state = runServer(port, dataPath, server);
+    const ephemeralServer = make<Delta, Data>(
+        crdtImpl,
+        setupInMemoryPersistence(),
+    );
+    setupWebsocket(state.app, ephemeralServer, '/ephemeral/sync');
+    setupPolling(state.app, ephemeralServer, '/ephemeral/sync');
+    return state;
 };
