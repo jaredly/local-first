4c79d0c651c00faf4f9f55d152cf60d99298c086
Jared Forsyth <jared@jaredforsyth.com>
2020-12-14 09:40:45 -0600

    auto alias flow! now things are nice

--- examples/tree-notes/auto-alias.js
+++ examples/tree-notes/auto-alias.js
@@ -8,9 +8,10 @@ const getImports = (text) => {
     const imports = {};
     text.replace(/\bfrom '([^']+)';\n/g, (_, path) => {
         if (path.startsWith('.')) {
-            return;
+            return '';
         }
         imports[path] = true;
+        return '';
     });
     return imports;
 };
@@ -36,8 +37,50 @@ const updatePackageJson = (filePath, imports) => {
     fs.writeFileSync(filePath, JSON.stringify(data, null, 4));
 };
 
-const updateFlowConfig = (filePath, imports) => {};
+const parseFlowConfig = (lines) => {
+    const sections = [];
+    lines.forEach((line) => {
+        if (line.match(/^\[.*\]$/)) {
+            sections.push([line]);
+            console.log(line);
+        } else if (sections.length) {
+            if (!line.trim() && sections[sections.length - 1].length === 1) {
+                // skip blanks right after header
+                return;
+            }
+            sections[sections.length - 1].push(line);
+        }
+    });
+
+    return sections;
+};
+
+const updateFlowConfig = (filePath, imports) => {
+    let sections = parseFlowConfig(fs.readFileSync(filePath, 'utf8').split('\n'));
+    sections = sections.map((section) => {
+        if (section[0].trim() !== '[options]') {
+            return section;
+        }
+        const newSection = [];
+        // const mappers = {}
+        section.forEach((line) => {
+            if (!line.startsWith(`module.name_mapper=`)) {
+                return newSection.push(line);
+            }
+            // mappers.push(line)
+        });
+        imports.forEach((name) => {
+            newSection.push(
+                `module.name_mapper='${name}' -> '<PROJECT_ROOT>/node_modules/${name}'`,
+            );
+        });
+        return newSection;
+    });
+    const newText = sections.map((section) => section.join('\n').trim()).join('\n\n');
+    fs.writeFileSync(filePath, newText, 'utf8');
+};
 
 const base = path.join(__dirname, '..', 'shared');
 const imports = getAllImports(base);
 updatePackageJson(path.join(__dirname, 'package.json'), imports);
+updateFlowConfig(path.join(__dirname, '.flowconfig'), imports);
